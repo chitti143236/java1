155b0c7dac493a32fc5f7145e26b031dffc8f7ca
chitti143236 <kadirelikitha@gmail.com>
2026-07-05 14:00:46 +0530

    java1

---
+++ app.java
@@ -0,0 +1,8 @@
+public student{
+public static void main(string[]args){
+	int id=100;
+	string name="venkat"
+		int age=22;
+	system.out.println("student id:"+id);
+	system.out.println("student name:"+name);
+system.out.println("student age:"+age);}}
