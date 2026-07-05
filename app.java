4821b925d67b0dc3f12f2b6dd36ebae517fa5a8d
chitti143236 <kadirelikitha@gmail.com>
2026-07-05 14:22:32 +0530

    app.java

--- app.java
+++ app.java
@@ -1,8 +1,8 @@
-public student{
+public vlass Student{
 public static void main(string[]args){
 	int id=100;
-	string name="venkat"
+	String name="venkat";
 		int age=22;
-	system.out.println("student id:"+id);
-	system.out.println("student name:"+name);
-system.out.println("student age:"+age);}}
+	System.out.println("student id:"+id);
+	System.out.println("student name:"+name);
+System.out.println("student age:"+age);}}
